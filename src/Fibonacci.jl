279ed8b46d0260c164c3cd186dc8cb3290d46834
singularitti <singularitti@outlook.com>
2023-11-05 01:41:50 -0500

    Fix the `iterate` interface

--- src/Fibonacci.jl
+++ src/Fibonacci.jl
@@ -8,17 +8,19 @@ struct FibonacciIterator
     end
 end
 
-Base.iterate(::FibonacciIterator) = ((0, 1), 2)
+eachfibbo(n) = FibonacciIterator(n)
+
+Base.iterate(::FibonacciIterator) = (1, ((0, 1), 2))
 function Base.iterate(iter::FibonacciIterator, ((fₙ₋₂, fₙ₋₁), n))
     if n > iter.maxiter
         return nothing
     else
         fₙ = fₙ₋₁ + fₙ₋₂
-        return (fₙ₋₁, fₙ), n + 1
+        return fₙ, ((fₙ₋₁, fₙ), n + 1)
     end
 end
 
-Base.eltype(::Type{FibonacciIterator}) = Tuple{UInt64,UInt64}
+Base.eltype(::Type{FibonacciIterator}) = Base.UInt128
 
 Base.length(iter::FibonacciIterator) = iter.maxiter
 
