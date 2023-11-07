dc12bbd51f722d5c03402c4a4092f5cf3421f60f
singularitti <singularitti@outlook.com>
2023-11-06 21:16:34 -0500

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,18 @@ using Fibonacci
 using Test
 
 @testset "Fibonacci.jl" begin
-    # Write your tests here.
+    @test_throws AssertionError FibonacciSequence(0)
+    @test_throws AssertionError FibonacciSequence(-1)
+    @test_throws BoundsError FibonacciSequence(10)[-1]
+    @test_throws BoundsError FibonacciSequence(10)[0]
+    @test_throws BoundsError FibonacciSequence(10)[11]
+
+    expected_values = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+    for (i, number) in enumerate(FibonacciSequence(10))
+        @test number == expected_values[i]  # Test the iteration interface
+        @test number == FibonacciSequence(10)[i]  # Test the indexing interface
+    end
+
+    @test FibonacciSequence(10)[1] == FibonacciSequence(10)[begin] == 1
+    @test FibonacciSequence(10)[10] == FibonacciSequence(10)[end] == 55
 end
